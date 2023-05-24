99f7fb63190e23b92abb247e8e87204862bf8d9d
MarielleVallee <marielle.vallee@gmail.com>
2023-05-24 14:40:31 +0200

    commit Environnement && Tour (first time)

---
+++ src/main/java/fr/iut/montreuil/saesprint1/modele/Environnement.java
@@ -0,0 +1,36 @@
+package fr.iut.montreuil.saesprint1.modele;
+
+import java.util.ArrayList;
+
+public class Environnement {
+
+    private ArrayList<Ennemi> ennemis;
+    private ArrayList<Tour> tours;
+    private Terrain terrain;
+
+    public Environnement(Terrain terrain) {
+        this.ennemis = new ArrayList<>();
+        this.tours = new ArrayList<>();
+        this.terrain = terrain;
+    }
+
+    public ArrayList<Ennemi> getEnnemis() {
+        return ennemis;
+    }
+
+    public void ajouterTour(Tour tour){
+        this.tours.add(tour);
+    }
+
+    public void ajouterEnnemi(Ennemi ennemi){
+        this.ennemis.add(ennemi);
+    }
+
+    public Terrain getTerrain() {
+        return terrain;
+    }
+
+    public ArrayList<Tour> getTours() {
+        return tours;
+    }
+}
---
+++ src/main/java/fr/iut/montreuil/saesprint1/modele/Tour.java
@@ -0,0 +1,71 @@
+package fr.iut.montreuil.saesprint1.modele;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public abstract class Tour {
+
+    private String nomTour;
+    private int cout;
+    private Environnement env;
+    private IntegerProperty x;
+    private IntegerProperty y;
+    public static int idTour = 0 ;
+
+    public Tour(String nomTour, int cout, int x, int y, Environnement env) {
+        this.nomTour = nomTour;
+        this.cout = cout;
+        this.x = new SimpleIntegerProperty(x);
+        this.y = new SimpleIntegerProperty(y);
+        this.env = env;
+        this.idTour = idTour;
+        idTour++;
+    }
+
+    public abstract void attaque();
+
+    public int getCout() {
+        return cout;
+    }
+
+    public final int getY() {
+        return this.y.get();
+    }
+
+    public final int getX() {
+        return this.x.get();
+    }
+
+    public final IntegerProperty getXProperty(){
+        return this.x;
+    }
+
+    public final IntegerProperty centreTourX(){
+        IntegerProperty a = new SimpleIntegerProperty(this.x.get()+16);
+        return a;
+    }
+
+    public final IntegerProperty centreTourY(){
+        IntegerProperty a = new SimpleIntegerProperty(this.y.get()+16);
+        return a;
+    }
+
+    public final IntegerProperty getYProperty(){
+        return this.y;
+    }
+
+    public String getId(){
+        return this.idTour + "";
+    }
+
+    @Override
+    public String toString() {
+        return "Tour{" +
+                "nomTour='" + nomTour + '\'' +
+                ", cout=" + cout +
+                ", x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+}
