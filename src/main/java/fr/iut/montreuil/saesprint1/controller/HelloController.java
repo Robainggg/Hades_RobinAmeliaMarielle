2225fb329b15a159496e37d954044d30c1a3b5ce
Robin Castel <robincastel2003@gmail.com>
2023-05-30 15:32:35 +0200

    Merge branche Robin avec Master

--- src/main/java/fr/iut/montreuil/saesprint1/controller/HelloController.java
+++ src/main/java/fr/iut/montreuil/saesprint1/controller/HelloController.java
@@ -48,8 +48,6 @@ public class HelloController implements Initializable {
 
     private Timeline gameLoop;
 
-    private int temps;
-
     private ListObsEnnemis listenerEnnemis;
 
     private Ennemi ennemi;
@@ -69,19 +67,16 @@ public class HelloController implements Initializable {
 
         evt.ajouterTour(tour);
         creerUneTour(tour);
-        evt.ajouterEnnemi(ennemi);
 
 
         initAnimation();
         gameLoop.play();
 
-        this.terrain.afficheTableau();
-
     }
 
     private void initAnimation() {
         gameLoop = new Timeline();
-        temps=0;
+        temps = 0;
         gameLoop.setCycleCount(Timeline.INDEFINITE);
 
         KeyFrame kf = new KeyFrame(
@@ -89,36 +84,20 @@ public class HelloController implements Initializable {
                 Duration.seconds(0.017),
                 // on définit ce qui se passe à chaque frame
                 // c'est un eventHandler d'ou le lambda
-                (ev ->{
-                    if(ennemi.estArriveAuBout()){
-                        System.out.println("fini");
-                        gameLoop.stop();
-                    }
-                    else{
-                        if(ennemi.estArrivé()) {
-                            if(ennemi.getBfs().getParcours().size()==0)
-                                gameLoop.stop();
-                            ennemi.changeProchaineCase();
-                            System.out.println(ennemi.getProchaineCase());
-                            if(ennemi.getProchaineCase() != null)
-                                ennemi.definirDirection();
-                            System.out.println(ennemi.getDirection());
+                (ev -> {
+                    for(int i = 0; i<this.evt.getEnnemis().size();i++){
+                        if (evt.getEnnemis().get(i).estArriveAuBout()) {
+                            System.out.println("fini");
+                            gameLoop.stop();
+                    } else {
+                        evt.getEnnemis().get(i).seDeplace();
                         }
-                        if(ennemi.getDirection() == 'd'){
-                            ennemi.setCoordX(ennemi.getCoordX()+2);
-                        }
-                        else if (ennemi.getDirection() == 'g'){
-                            ennemi.setCoordX(ennemi.getCoordX()-2);
-                        }
-                        else if (ennemi.getDirection() == 'h'){
-                            ennemi.setCoordY(ennemi.getCoordY()-2);
-                        }
-                        else if (ennemi.getDirection() == 'b'){
-                            ennemi.setCoordY(ennemi.getCoordY()+2);
-                        }
-                        //System.out.println(ennemiTesté.estArrivé() + " ennemi a pour coordonnées: " + ennemiTesté.getCoordX() + " , " + ennemiTesté.getCoordY() + " et pour destination " + (ennemiTesté.getProchaineCase().getI()*32-16) + " ," + (ennemiTesté.getProchaineCase().getJ()*32-16));
 
                     }
+
+                    //System.out.println(ennemiTesté.estArrivé() + " ennemi a pour coordonnées: " + ennemiTesté.getCoordX() + " , " + ennemiTesté.getCoordY() + " et pour destination " + (ennemiTesté.getProchaineCase().getI()*32-16) + " ," + (ennemiTesté.getProchaineCase().getJ()*32-16));
+
+
                     temps++;
                 })
         );
